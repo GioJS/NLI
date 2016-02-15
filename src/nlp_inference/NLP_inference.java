34885bd873f792978ed0fd6b547d817b7bb0ece7
giordanocristini <giordano.cristini@gmail.com>
2016-02-15 15:46:59 +0100

    ya

---
+++ src/nlp_inference/NLP_inference.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nlp_inference;
+import it.uniroma2.dtk.dt.GenericDT;
+import it.uniroma2.util.tree.Tree;
+import it.uniroma2.tk.TreeKernel;
+import java.io.File;
+/**
+ *
+ * @author giordanocristini
+ */
+public class NLP_inference {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
