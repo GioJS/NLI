394acf434f198a3f4312ae52e9f5cdc887e2d564
giordanocristini <giordano.cristini@gmail.com>
2016-02-16 09:35:19 +0100

    fix'd

--- src/nlp_inference/NLP_inference.java
+++ src/nlp_inference/NLP_inference.java
@@ -9,6 +9,7 @@ import it.uniroma2.util.tree.Tree;
 import it.uniroma2.tk.TreeKernel;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 /**
  *
  * @author giordanocristini
@@ -18,7 +19,7 @@ public class NLP_inference {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, Exception {
         // TODO code application logic here
         //leggere dal csv separato da tab (\t)
         //per ogni riga estrarre i due alberi parentesizzati
@@ -26,10 +27,11 @@ public class NLP_inference {
         //generare i dt
         //dot product tra dt
         //abbiamo similitudine tra le due frasi
-        String filename="C:\\Users\\Caterina\\Documents\\NetBeansProjects\\NLI\\snli_1.0_dev.txt";
+        String filename="snli_1.0_dev.txt";
         CSVParser parser=new CSVParser(filename);
-        
-        parser.parse();
+        List<Tree> pair=null;
+        while((pair=parser.nextPair()).size()!=0)
+            System.out.println(pair);
     }
     
 }
